e768e76286eb51c64baa4abcd3cecfbb70e31095
HuangZhenchao <1746565921@qq.com>
2022-04-01 17:55:17 +0800

    项目重构

--- py3-htmlParse/test-python-chm.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python  
-# -*- coding:utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-from pythonChm.pythonChm import pythonCHM
-pyCHM=pythonCHM(u'D:\\book')
-pyCHM.excute()
